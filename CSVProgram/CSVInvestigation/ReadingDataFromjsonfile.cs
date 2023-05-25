510b264e254b35e7adec039f4fcf3b7fe478b2e7
DSaijyothi <saijyothid12@gmail.com>
2023-05-25 17:13:31 +0530

    json file reading

---
+++ CSVProgram/CSVInvestigation/ReadingDataFromjsonfile.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVProgram.CSVInvestigation
+{
+    public class Datum
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public List<string> Jobs { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Datum> Data { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+
+    public class Skill
+    {
+        public string Web { get; set; }
+        public string Mobile { get; set; }
+    }
+
+    public class ReadingDataFromjsonfile
+    {
+        static void Main(string[] args)
+        {
+            string link = @"C:\Users\Sai Jyothi\Desktop\Data.json";
+            WebRequest request =  WebRequest.Create(link);
+            WebResponse response=request.GetResponse();
+
+            using(Stream datastream= response.GetResponseStream())
+            {
+                StreamReader reader=new StreamReader(datastream);
+
+                string responseFromServer=reader.ReadToEnd();
+
+                Root root=JsonConvert.DeserializeObject<Root>(responseFromServer);
+
+                foreach(Datum d in root.Data)
+                    {
+                    Console.WriteLine(d.Name+" "+d.Surname+",Age:"+d.Age+",he work as:");
+                    
+                    for(int i=0;i<d.Jobs.Count;i++)
+                    {
+                        Console.WriteLine(d.Jobs[i]);
+                    }
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CSVProgram/CSVInvestigation/ReadingDataFronCSVStudentFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVProgram.CSVInvestigation
+{
+    internal class ReadingDataFronCSVStudentFile
+    {
+
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\Sai Jyothi\Desktop\CollegeData.Csv";
+            try
+            {
+                using (StreamReader reader = new StreamReader(filepath)) ;
+                {
+                    string line;
+                    while((line= ReadingDataFronCSVStudentFile.ReadLine())!=null)
+                    {
+                        Console.WriteLine(line);
+                    }    
+                }
+            }
+           catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        private static string? ReadLine()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
